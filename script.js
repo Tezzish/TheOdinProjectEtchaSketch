7b41840007ba9ea1bd0c7c347a4f82a13fc5544c
Tezzish <ishanpahwa14@gmail.com>
2022-08-15 21:49:45 +0530

    Refactored the code into classes

--- script.js
+++ script.js
@@ -1,97 +1,111 @@
-//all the elements that we need for now
-const canvasContainer = document.getElementById("canvasContainer");
-const bodyarr = document.getElementsByTagName("body");
-const body = bodyarr.item(0);
-const eraser = document.getElementById("eraser");
-const resetb = document.getElementById("reset");
+class Canvas{
 
-//checks if the mouse is pressed on the body and allows the user to draw
-let mousebool = false;
-
-body.addEventListener("mousedown", () => {
-    mousebool = true;
-    console.log("started");
-    event.preventDefault();
-});
-body.addEventListener("mouseup", () => {
-    mousebool = false;
-    console.log("ended");
-});
-
-
-//used for the erase to allow it to be toggled
-let prev_colour = "black";
-let colour = "black";
-let eraserBool = false;
-
-//gets the number of cells on each side of the canvas
-let cells = 16;
-//squares the number of cells on each side of the canvas to get the total number of cells
-let nocells = Math.pow(cells, 2);
-//sets wh to the maximum width/height of the cells
-let wh = 720/nocells;
-
-//eraser for the cells
-
-eraser.onclick = () => {
-    if(eraserBool){
-        colour = prev_colour;
-        eraserBool = !eraserBool;
+    //constructor
+    //takes the number of pixels each side as a parameter
+    constructor(pix){
+        //gets the canvas element
+        this.canvasContainer = document.getElementById("canvasContainer");
+        //gets the body element
+        this.body = document.getElementsByTagName("body").item(0);
+        //gets the eraser button
+        this.eraser = document.getElementById("eraser");
+        //gets the reset button
+        this.resetb = document.getElementById("reset");
+        //number of cells on each side
+        this.cells = pix;
+        //sets the mouse boolean to false because it is currently not being clicked
+        this.mousebool = false;
+        //used for the eraser function
+        this.prev_colour = "black";
+        this.colour = "black";
+        this.eraserBool = false;
+        this.divs;
     }
-    else{
-        colour = "white";
-        eraserBool = !eraserBool;
-    }
-}
 
-//arranges the cells in the canvas
-let auto = "";
+    //sets up the canvas with the buttons 
+    setUp(){
+        //checks if the mouse is pressed on the body and allows the user to draw
+        //starts drawing if the mouse button is pressed
+        this.body.addEventListener("mousedown", () => {
+            this.mousebool = true;
+            console.log("started");
+            event.preventDefault();
+        });
 
-for(var i = 0; i < cells; i++){
-    if(i == cells-1){
-        auto += "auto";
-    }
-    else{
-        auto += "auto ";
-    }
-}
-
-canvasContainer.style.gridTemplateColumns = auto;
-
-//creates the cells of the canvas 
-for(var i=0; i<nocells; i++) {
-    const child = document.createElement("div");
-    const list = child.classList;
-    list.add("grid-item");
-    let id = "item" + i;
-    child.id = id;
-    canvasContainer.appendChild(child);
-}
-
-const divs = document.querySelectorAll("div.grid-item");
-
-divs.forEach((child)=>{
-    child.addEventListener("mouseover", () =>{
-        if(mousebool){
-            console.log("should be working");
-            child.style.backgroundColor = colour;
+        //stops drawing if the mouse button is not pressed
+        this.body.addEventListener("mouseup", () => {
+            this.mousebool = false;
+            console.log("ended");
+        });
+    
+        //eraser logic for the button
+        this.eraser.onclick = () => {
+            if(this.eraserBool){
+                this.colour = this.prev_colour;
+                this.eraserBool = !this.eraserBool;
+            }
+            else{
+                this.colour = "white";
+                this.eraserBool = !this.eraserBool;
+            }
         }
-    })
-    child.addEventListener("mousedown", () =>{
-        child.style.backgroundColor = colour;
-    });
-})
+        //squares the number to get the total number of cells in the canvas
+        let nocells = Math.pow(this.cells, 2);
+        //creates the cells of the canvas 
+        for(var i=0; i<nocells; i++) {
+            const child = document.createElement("div");
+            const list = child.classList;
+            list.add("grid-item");
+            let id = "item" + i;
+            child.id = id;
+            canvasContainer.appendChild(child);
+        }
+        //arranges the cells in the canvas
+        let auto = "";
+        for(var i = 0; i < this.cells; i++){
+            if(i == this.cells-1){
+                auto += "auto";
+            }
+            else{
+                auto += "auto ";
+            }
+        }
+        canvasContainer.style.gridTemplateColumns = auto;
 
-function reset(){
-    divs.forEach((child)=>{
-        child.style.backgroundColor = "white";
-    })
-}
+        this.resetb.onclick = ()=>{
+            let yn = prompt("Are you sure you want to reset? (yes/no)");
+            yn = yn.toLowerCase();
+            if(yn == "yes" || yn == "y"){
+                this.reset();
+            }
+        }
 
-resetb.onclick = ()=>{
-    let yn = prompt("Are you sure you want to reset? (yes/no)");
-    yn = yn.toLowerCase();
-    if(yn == "yes" || yn == "y"){
-        reset();
+        this.divsSetUp();
+    }
+
+    //adds the function to draw to the cells
+    divsSetUp(){
+        this.divs = document.querySelectorAll("div.grid-item");
+    
+        this.divs.forEach((child)=>{
+            child.addEventListener("mouseover", () =>{
+                if(this.mousebool){
+                    child.style.backgroundColor = this.colour;
+                } 
+            })
+            child.addEventListener("mousedown", () =>{
+                child.style.backgroundColor = this.colour;
+            });
+        });
+    }
+
+    //resets the canvas to white
+    reset(){
+        this.divs.forEach((child)=>{
+            child.style.backgroundColor = "white";
+        })
     }
 }
+
+let canvas = new Canvas(16);
+canvas.setUp();
